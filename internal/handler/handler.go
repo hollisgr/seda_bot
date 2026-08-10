a01341fd5414cb4f343238b712901ba6618c7eb6
hollisgr <hollisgr@student.21-school.ru>
2026-08-11 00:43:16 +0900

    start handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -14,11 +15,22 @@ func New() *Handler {
 }
 
 func (h *Handler) Register(b *bot.Bot) {
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, h.StartHandler)
 }
 
 func (h *Handler) Default(ctx context.Context, b *bot.Bot, update *models.Update) {
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
 		Text:   update.Message.Text,
+	})
+}
+
+func (h *Handler) StartHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	firstName := update.Message.From.FirstName
+	text := fmt.Sprintf("Hello %s!\nWelcome to SedaBot!", firstName)
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   text,
 	})
 }
