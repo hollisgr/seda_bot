48e0ce31daab54886a441f33aef18de5d3c65242
Hollis Grigg <hollisgr@si-a5.yks.21-school.ru>
2026-08-18 15:03:27 +0900

    event handler fix

--- internal/handler/event.go
+++ internal/handler/event.go
@@ -7,8 +7,8 @@ import (
 	"github.com/go-telegram/bot/models"
 )
 
-func (h *Handler) SaveEvent(ctx context.Context, b *bot.Bot, update *models.Update)
+func (h *Handler) SaveEvent(ctx context.Context, b *bot.Bot, update *models.Update) {}
 
-func (h *Handler) LoadEvent(ctx context.Context, b *bot.Bot, update *models.Update)
+func (h *Handler) LoadEvent(ctx context.Context, b *bot.Bot, update *models.Update) {}
 
-func (h *Handler) LoadActualEvents(ctx context.Context, b *bot.Bot, update *models.Update)
+func (h *Handler) LoadActualEvents(ctx context.Context, b *bot.Bot, update *models.Update) {}
