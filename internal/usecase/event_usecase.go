ae696dfacd266ae60a8c2f23fcb6ceebb7a79830
Hollis Grigg <hollisgr@si-a5.yks.21-school.ru>
2026-08-18 14:28:15 +0900

    add event usecases

--- internal/usecase/event_usecase.go
+++ internal/usecase/event_usecase.go
@@ -1,6 +1,14 @@
 package usecase
 
+import (
+	"context"
+	"sedabot/internal/model"
+)
+
 type EventRepository interface {
+	Save(ctx context.Context, event model.Event) (int, error)
+	Load(ctx context.Context, id int) (model.Event, error)
+	LoadActiveEvents(ctx context.Context) ([]model.Event, error)
 }
 
 type EventUseCase struct {
@@ -11,4 +19,28 @@ func NewEventUseCase(eventRepo EventRepository) *EventUseCase {
 	return &EventUseCase{
 		eventRepo: eventRepo,
 	}
+}
+
+func (u *EventUseCase) SaveEvent(ctx context.Context, event model.Event) (int, error) {
+	id, err := u.eventRepo.Save(ctx, event)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func (u *EventUseCase) LoadEvent(ctx context.Context, id int) (model.Event, error) {
+	event, err := u.eventRepo.Load(ctx, id)
+	if err != nil {
+		return model.Event{}, err
+	}
+	return event, nil
+}
+
+func (u *EventUseCase) LoadActiveEvents(ctx context.Context) ([]model.Event, error) {
+	events, err := u.eventRepo.LoadActiveEvents(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return events, nil
 }
